57cbb48df912d2f6d202332a283df820c9190860
SpicyTalha <spicytalhadevelops@gmail.com>
2025-05-19 13:16:19 +0500

    feat: created env in fronetend

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const environment = import.meta.env.VITE_ENVIRONMENT || "local"
+const environment = import.meta.env.VITE_ENVIRONMENT || "preview"
 let API_BASE = ""
 if (environment === "preview")
     API_BASE = "http://ec2-34-204-7-231.compute-1.amazonaws.com:8000";
